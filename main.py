c495f118d08cd69f61ad02456853a22f6c8a5325
gnata <grahovanatalya@gmail.com>
2024-11-07 15:11:28 +0300

    Добавлена автоматическое копирование в буфер обмена

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from  tkinter import messagebox as mb
 from tkinter import ttk
 import requests
+import pyperclip
 
 
 def upload():
-    filepath = fd.askopenfilename()
-    if filepath:
-        files = {"file": open(filepath, "rb")}
-        response = requests.post("https://file.io", files=files)
-        if response.status_code == 200:
-            link = response.json()["link"]
-            entry.insert(0, link)
-
-
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, "rb") as f:
+                files = {"file": f}
+                response = requests.post("https://file.io", files=files)
+                response .raise_for_status()
+                link = response.json()["link"]
+                entry.delete(0, END)
+                entry.insert(0, link)
+                pyperclip.copy(link)
+                mb.showinfo("Ссылка скопирована", f"Ссылка {link} успешно скопирована в буфер обмена")
+    except Exception as e:
+        mb.showerror("Ошибка!", f"Произошла ошибка: {e}")
 
 
 window = Tk()
 window.title("Сохраните файл в облаке ")
 window.geometry("400x200")
 
-button = ttk.Button(text="Загрузить файл", command=upload)
-button.pack()
-# создаем поле ввода выводит ссылку на скопир файл
+upload_button = ttk.Button(text="Загрузить файл", command=upload)
+upload_button.pack()
+
 entry = ttk.Entry()
 entry.pack()
+
+window.mainloop()
+
 
